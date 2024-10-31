eb419bf8597a0287d13d3c0246f4fe46a00d78a8
Krishna Singh <id.krishnasingh@gmail.com>
2024-10-31 22:51:59 +0530

    git commit -m "impr: create accunt endpoint"

---
+++ src/main/java/com/microservices/bank_accounts/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.microservices.bank_accounts.exception;
+
+import com.microservices.bank_accounts.dto.ErrorDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
+    public ResponseEntity<ErrorDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException ex, WebRequest req){
+        ErrorDto errorResponse = new ErrorDto(req.getContextPath(), HttpStatus.BAD_REQUEST, ex.getMessage(), LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+}
