a97010be5bc446ba7d2f2657807833d6bd564fe8
kks-it <id.krishnasingh@gmail.com>
2024-11-01 13:16:18 +0530

    added global exception handler

--- src/main/java/com/microservices/bank_accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/com/microservices/bank_accounts/exception/GlobalExceptionHandler.java
@@ -13,15 +13,26 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomerAlreadyExistsException.class)
-    public ResponseEntity<ErrorDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException ex, WebRequest req){
+    public ResponseEntity<ErrorDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException ex, WebRequest req) {
         ErrorDto errorResponse = new ErrorDto(req.getContextPath(), HttpStatus.BAD_REQUEST, ex.getMessage(), LocalDateTime.now());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorDto> handleResouceNotFoundException(ResourceNotFoundException ex, WebRequest req){
-        ErrorDto error = new ErrorDto(req.getDescription(false), HttpStatus.NOT_FOUND, ex.getMessage() , LocalDateTime.now());
+    public ResponseEntity<ErrorDto> handleResouceNotFoundException(ResourceNotFoundException ex, WebRequest req) {
+        ErrorDto error = new ErrorDto(req.getDescription(false), HttpStatus.NOT_FOUND, ex.getMessage(), LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleGlobalException(Exception ex, WebRequest req) {
+        ErrorDto error = new ErrorDto(
+                req.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
